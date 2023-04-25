85a46e4043cdbd2367d0fc57dc0a8e48627ec442
AtanasS3 <127635571+AtanasS3@users.noreply.github.com>
2023-04-25 11:53:21 +0300

    Add files via upload

---
+++ week8.cpp
@@ -0,0 +1,37 @@
+// ConsoleApplication16.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+/*template <typename T>
+T swap(T& a, T& b)
+{
+	T c;
+	c = a;
+	a = b;
+	b = c;
+	return 0;
+}
+*/
+
+template<typename T>
+class Set
+{
+public:
+	Set();
+	Set(int value);
+	Set(cosnt Set&);
+	~Set();
+private:
+	T* arr = new T[100];
+
+	
+};
+
+
+int main()
+{
+	
+}
+
+
